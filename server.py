c06b6bf33be83ce854f0560e2f45cb323e176365
TejasShirsath <tejasshirsath5@gmail.com>
2025-02-03 01:54:58 +0530

    Change gemini-pro to gemini-2.0-flash-exp

--- server.py
+++ server.py
@@ -2,31 +2,31 @@ from flask import Flask, request
 from urllib.parse import unquote
 import google.generativeai as genai
 from flask_cors import CORS
+from dotenv import load_dotenv
 import os
 
-from dotenv import load_dotenv
 load_dotenv()
-#remove this both 6th and 7th line of code
 
-genai.configure(api_key = os.getenv("api_key")) #Replace 'os.getenv("api_key")' with your Google Genmi API Key
+# Replace 'os.getenv("api_key")' with your actual Gemini API Key
+genai.configure(api_key = os.getenv("api_key"))
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/', methods=['GET'])
 def get_answer():
-    question =request.args.get('question')
+    question = request.args.get('question')
     decoded_question = unquote(question) if question else ""
     answer = generate_answer(decoded_question)
     return answer
 
 def generate_answer(question):
     try:
-        model = genai.GenerativeModel('models/gemini-pro')
-        response = model.generate_content(question).text
-        return response
-    except:
+        model = genai.GenerativeModel(model_name="gemini-2.0-flash-exp")
+        response = model.generate_content(question)
+        return response.text
+    except Exception as e:
         return "Sorry, I'm unable to provide an answer."
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
